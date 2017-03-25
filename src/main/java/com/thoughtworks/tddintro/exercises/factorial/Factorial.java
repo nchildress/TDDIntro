983108f70e804937dba4bf80386392b8bd5d2f97
nchildress <nchildress@smu.edu>
2017-03-25 14:07:23 -0500

    pass all tests

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -1,7 +1,8 @@
 package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
-    public Integer compute(int i) {
+    public Integer compute(int i) throws IllegalArgumentException {
+        if(i < 0) throw new IllegalArgumentException();
         if(i < 1) return 1;
         else
         return i * compute(i - 1);
